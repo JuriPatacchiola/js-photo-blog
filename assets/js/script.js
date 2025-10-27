7ebebbdbc95855abdc05b38d4321246fad76eba4
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-10-27 17:18:28 +0100

    add the referement for my url and grid

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,8 @@
 
     document.addEventListener('DOMContentLoaded', () => {
 
-        
+        // - prendo i riferimenti per l'url che ho usato dentro postman e il ID per il griled 
+    const galleryContainer = document.getElementById('photo-gallery'); 
+    const apiUrl = 'https://lanciweb.github.io/demo/api/pictures/';
 
     });
