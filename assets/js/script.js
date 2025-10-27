41f258f8361d26c7776ef583d01dfec3321aa799
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-10-27 17:16:55 +0100

    add the eventlistener for the create and recall all element

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+/**
+ * leggenda
+    - Argomento principale / Azione / Funzione
+    -- Argomento secondario / Ragionamento interno
+ */
+//-creo il docunet con  addeventlistener per crearmi l'evento principaleche terra dentro tutte le funzioni e elementi
+
+    document.addEventListener('DOMContentLoaded', () => {
+
+        
+
+    });
